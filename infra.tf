d4df17ed6644573908bda687e09a4d4881eeb38e
Jagadish <Jagadishdevops.omega@gmail.com>
2023-12-20 12:02:48 +0530

    Added Terraform code

--- infra.tf
+++ infra.tf
@@ -8,6 +8,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = Z012136531Q4PFU8T7ZYB
+  name    = "frontend-dev.jagadish.cloud"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
@@ -16,6 +24,14 @@ resource "aws_instance" "mongodb" {
   tags = {
     Name = "mongodb"
   }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = Z012136531Q4PFU8T7ZYB
+  name    = "mongodb-dev.jagadish.cloud"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
 }
 
 resource "aws_instance" "catalogue" {
@@ -28,6 +44,14 @@ resource "aws_instance" "catalogue" {
   }
 }
 
+resource "aws_route53_record" "catalogue" {
+  zone_id = Z012136531Q4PFU8T7ZYB
+  name    = "catalogue-dev.jagadish.cloud"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
 resource "aws_instance" "redis" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
@@ -36,6 +60,14 @@ resource "aws_instance" "redis" {
   tags = {
     Name = "redis"
   }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = Z012136531Q4PFU8T7ZYB
+  name    = "redis-dev.jagadish.cloud"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
 }
 
 resource "aws_instance" "user" {
@@ -48,6 +80,14 @@ resource "aws_instance" "user" {
   }
 }
 
+resource "aws_route53_record" "user" {
+  zone_id = Z012136531Q4PFU8T7ZYB
+  name    = "user-dev.jagadish.cloud"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
 resource "aws_instance" "cart" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
@@ -56,6 +96,14 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = Z012136531Q4PFU8T7ZYB
+  name    = "cart-dev.jagadish.cloud"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "mysql" {
@@ -68,6 +116,14 @@ resource "aws_instance" "mysql" {
   }
 }
 
+resource "aws_route53_record" "mysql" {
+  zone_id = Z012136531Q4PFU8T7ZYB
+  name    = "mysql-dev.jagadish.cloud"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
 resource "aws_instance" "shipping" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
@@ -76,6 +132,14 @@ resource "aws_instance" "shipping" {
   tags = {
     Name = "shipping"
   }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = Z012136531Q4PFU8T7ZYB
+  name    = "mysql-dev.jagadish.cloud"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
 }
 
 resource "aws_instance" "rabbitmq" {
@@ -88,6 +152,14 @@ resource "aws_instance" "rabbitmq" {
   }
 }
 
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = Z012136531Q4PFU8T7ZYB
+  name    = "rabbitmq-dev.jagadish.cloud"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
 resource "aws_instance" "payment" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
@@ -96,4 +168,12 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = Z012136531Q4PFU8T7ZYB
+  name    = "payment-dev.jagadish.cloud"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
 }
