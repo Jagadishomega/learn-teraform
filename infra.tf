3b84ead713a0ee39564829e69d1581f4838efd72
Jagadish <Jagadishdevops.omega@gmail.com>
2023-12-20 11:35:46 +0530

    Added Terraform code

--- infra.tf
+++ infra.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
+  security_groups = [ "sg-074e4e2e7a2eec422" ]
 
   tags = {
     Name = "frontend"
